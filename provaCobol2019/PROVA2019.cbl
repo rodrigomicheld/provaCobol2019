2d52fbb4266655a3dd8ca2171b1d8a99337e76ab
user <user@DESKTOP-QK21JNK>
2019-03-01 22:17:23 -0300

    Importação do projeto no GIT

---
+++ provaCobol2019/PROVA2019.cbl
@@ -0,0 +1,14 @@
+       program-id. PROVA2019 as "PROVA2019".
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+             
+
+           goback.
+
+       end program PROVA2019.
